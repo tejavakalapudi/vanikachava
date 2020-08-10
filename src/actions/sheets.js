643d75a5328305c980c132c98f23fad791a1eb31
Raviteja Vakalapudi <tejavakalapudi@gmail.com>
2020-08-10 09:23:44 +0530

    Decoupling Rows data for analytics

--- src/actions/sheets.js
+++ src/actions/sheets.js
@@ -1,4 +1,6 @@
+/* eslint-disable radix */
 import GoogleSheet from '../google-api/sheets';
+import initAnalytics from './analytics';
 
 const setOrdersInfo = (payload) => ({
   type: 'SET_ORDERS',
@@ -14,8 +16,15 @@ const fetchLatestDataFromSheets = () => async (dispatch) => {
   const sheet = GoogleSheet.sheetsByIndex[0];
   const rows = await sheet.getRows();
 
-  dispatch(setContacts(rows.map((x) => x.contact)));
-  dispatch(setOrdersInfo(rows));
+  const formattedRows = rows.map((x) => ({
+    ...x,
+    quote_price: parseInt(x.quote_price),
+    inventory_costing: parseInt(x.inventory_costing),
+  }));
+
+  dispatch(setContacts(formattedRows.map((x) => x.contact)));
+  dispatch(setOrdersInfo(formattedRows));
+  dispatch(initAnalytics(formattedRows));
 };
 
 export default fetchLatestDataFromSheets;
---
+++ src/reducers/analytics.js
@@ -0,0 +1,26 @@
+const initialState = { current: 0, previous: 0, total: 0 };
+
+const defaultAnalytics = {
+  sale: initialState,
+  expense: initialState,
+  orders: initialState,
+  customers: initialState,
+  pending: 0,
+};
+
+export default (state = defaultAnalytics, action) => {
+  switch (action.type) {
+    case 'SET_SALE':
+      return { ...state, sale: action.payload };
+    case 'SET_EXPENSE':
+      return { ...state, expense: action.payload };
+    case 'SET_ORDERS':
+      return { ...state, orders: action.payload };
+    case 'SET_CUSTOMERS':
+      return { ...state, customers: action.payload };
+    case 'SET_PENDING':
+      return { ...state, pending: action.payload };
+    default:
+      return state;
+  }
+};
